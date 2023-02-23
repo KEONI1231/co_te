f77651e3da82d3ea4f2586c5826515719a15db85
keoni <kimkeonhwi991231@gmail.com>
2023-02-23 18:08:11 +0900

    5014

--- Solved_Ac_Code/1697/1697.py
+++ Solved_Ac_Code/1697/1697.py
@@ -1,10 +1,23 @@
+
 from collections import deque
 
 a,b = map(int, input().split())
-max = 10**5
-result = [0] * (max)
+max = 10 ** 5
+result = [0] * (max + 1)
+
 
 def bfs():
     que = deque()
     que.append(a)
+    while que:
+        node = que.popleft()
+        if node == b:
+            print(result[node])
+            break
+        for i in (node -1, node+1, node *2):
+            if 0 <= i <= max and not result[i]:
+                result[i] = result[node] + 1
+                que.append(i)
+
+bfs()
 
